7fd714e61f557ea65c19a4b4b79b0a075c2010e4
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-04-27 22:05:20 +0600

    my card details page added

---
+++ src/Pages/MyCardDetails.jsx
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+import { FaRegStar } from "react-icons/fa";
+import { MdOutlineEdit } from "react-icons/md";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+const MyCardDetails = ({ item }) => {
+  const { photo, item_name, rating, price, processing_time, customization, stock_status } = item;
+  return (
+    <div>
+      <div className="card card-compact bg-base-100 shadow-xl h-full">
+        <figure>
+          <img
+            className="transition duration-300 hover:scale-105"
+            src={photo}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="text-4xl font-bold mt-4 font-garamond bg-gradient-to-r from-[#4facfe] to-blue-500 bg-clip-text text-transparent">
+            {item_name}
+          </h2>
+          
+          <div className="flex">
+            <p className="text-lg font-medium opacity-70">$ {price}</p>
+            <p className="text-lg font-medium opacity-70 flex items-center gap-2">
+              {rating} <FaRegStar />
+            </p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+          <p className="text-lg opacity-70 font-medium">customization : {customization}</p>
+          <p className="text-lg opacity-70 font-medium">Stuck Status : {stock_status}</p>
+          </div>
+          
+          <p className="text-lg font-medium opacity-70 flex items-center gap-2">
+            Proccessing Time: {processing_time}
+          </p>
+          <div className=" flex gap-3">
+            <button className="flex justify-center items-center btn bg-blue-500 text-white hover:bg-blue-700 mt-6">
+            <MdOutlineEdit className="text-xl" /> Update Items
+            </button>
+            <button className="btn bg-red-500 text-white hover:bg-red-700 mt-6">
+            <RiDeleteBin2Fill className="text-xl" /> Delete 
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+MyCardDetails.propTypes = {
+    item: PropTypes.object,
+  };
+export default MyCardDetails;
--- src/Pages/MyCarft.jsx
+++ src/Pages/MyCarft.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../Hooks/useAuth";
+import MyCardDetails from "./MyCardDetails";
 
 const MyCarft = () => {
+    const [myItems, setMyItems]=useState([])
     const {user}=useAuth()
-    console.log(user)
     useEffect(()=>{
-        fetch(`http://localhost:3000/myCarftItems/${user.email}`)
-    },[])
+        fetch(`http://localhost:3000/myCarftItems/${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            setMyItems(data)
+        })
+    },[user])
     return (
-        <div>
-            <h1>my carft</h1>
+        <div className="container mx-auto p-4">
+            <h1>my carft {myItems.length}</h1>
+            <div className="grid lg:grid-cols-3 gap-6 mt-14">
+            {
+                myItems.map(item=><MyCardDetails key={item._id} item={item}></MyCardDetails>)
+            }
+            </div>
         </div>
     );
 };
