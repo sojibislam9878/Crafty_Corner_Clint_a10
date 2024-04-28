4c81347fa61a7a1fbd364aacc938412fe047def0
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-04-29 02:37:09 +0600

    filter dropdown added

--- src/Pages/MyCarft.jsx
+++ src/Pages/MyCarft.jsx
@@ -5,35 +5,73 @@ import NoData from "../Components/NoData";
 import Spinner from "../Components/Spinner";
 
 const MyCarft = () => {
-    const [myItems, setMyItems]=useState([])
-    const [reRender, setReRender]=useState(false)
-    const [sppinng, setSpinng] =useState(true)
-    const {user}=useAuth()
-    useEffect(()=>{
-        fetch(`http://localhost:3000/myCarftItems/${user?.email}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            setMyItems(data)
-            setSpinng(false)
-        })
-    },[user, reRender])
-    if (sppinng) {
-        return <Spinner></Spinner>
-        
-      }
-    if (myItems.length === 0) {
-        return <NoData></NoData>
-    }
-    return (
-        <div className="container mx-auto p-4">
-            <div className="grid lg:grid-cols-3 gap-6 mt-14">
-            {
-                myItems.map(item=><MyCardDetails key={item._id} item={item} reRender={reRender} setReRender={setReRender}></MyCardDetails>)
-            }
-            </div>
-        </div>
-    );
+  const [myItems, setMyItems] = useState([]);
+  const [reRender, setReRender] = useState(false);
+  const [sppinng, setSpinng] = useState(true);
+  const { user } = useAuth();
+  useEffect(() => {
+    fetch(`http://localhost:3000/myCarftItems/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMyItems(data);
+        setSpinng(false);
+      });
+  }, [user, reRender]);
+  if (sppinng) {
+    return <Spinner></Spinner>;
+  }
+  if (myItems.length === 0) {
+    return <NoData></NoData>;
+  }
+
+  const filter=(e)=>{
+      const value = e.target.value
+
+      fetch(`http://localhost:3000/craftitems?customization=${value}&&email=${user.email}`)
+      .then(res=>res.json())
+      .then(data=>{
+          console.log(data);
+          setMyItems(data)
+      })
+
+  }
+
+//   const find=(e)=>{
+//       console.log(e.target.value);
+//   }
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex justify-center items-center mt-3">
+        <p> </p>
+        <select onClick={filter} className="px-6 py-3 border bg-base-200 rounded-xl text-xl font-semibold">
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                </select>
+        {/* <details onClick={find} className="dropdown">
+          <summary className="m-1 btn">open or close</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details> */}
+      </div>
+      <div className="grid lg:grid-cols-3 gap-6 mt-14">
+        {myItems.map((item) => (
+          <MyCardDetails
+            key={item._id}
+            item={item}
+            reRender={reRender}
+            setReRender={setReRender}
+          ></MyCardDetails>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyCarft;
